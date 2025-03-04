3285d39cdd07abae9d5424966d858225203c3a12
unknown <YY338@ccs.local>
2025-03-04 05:21:09 -0800

    initial

---
+++ src/entity/User.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column()
+    role: string
+
+    @Column()
+    email : string
+
+    @Column()
+    password : string
+
+    @Column()
+    confimPassword: string
+}
